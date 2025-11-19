2845d2184d1f38a61041c002fefa8d3fcfae0cd7
ShemLeon <shemiakinleonid@gmail.com>
2025-11-19 22:33:41 +0200

    Bottom Nav. 13:00. 8 lesson

---
+++ app/src/main/java/com/leoevg/mihnewsapp/presentation/screen/main/profile/ProfileScreen.kt
@@ -0,0 +1,24 @@
+package com.leoevg.mihnewsapp.presentation.screen.main.profile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProfileScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Profile Screen",
+            fontSize = 30.sp
+        )
+    }
+}
