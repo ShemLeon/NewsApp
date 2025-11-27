fd3bcc9bc694c07ea7163eb1b5836dcd1fcc7364
ShemLeon <shemiakinleonid@gmail.com>
2025-11-27 23:16:20 +0200

    UI 12 lesson 14:00

--- app/src/main/java/com/leoevg/mihnewsapp/presentation/screen/main/feed/FeedScreenViewModel.kt
+++ app/src/main/java/com/leoevg/mihnewsapp/presentation/screen/main/feed/FeedScreenViewModel.kt
@@ -1,13 +1,21 @@
 package com.leoevg.mihnewsapp.presentation.screen.main.feed
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.leoevg.mihnewsapp.data.repository.NewsRepository
+import com.leoevg.mihnewsapp.domain.model.NewsItem
 import com.leoevg.mihnewsapp.presentation.navigation.Screen
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @HiltViewModel
 class FeedScreenViewModel @AssistedInject constructor(
@@ -16,9 +24,36 @@ class FeedScreenViewModel @AssistedInject constructor(
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(FeedScreenState())
-    @AssistedFactory
-    interface Factory{
-        fun create(navigate: (Screen) -> Unit): FeedScreenViewModel
+    val state = _state.asStateFlow()
+
+    private var news: List<NewsItem> = emptyList()
+
+
+    fun onEvent(event: FeedScreenEvent) {
+        when (event) {
+            is FeedScreenEvent.NewsItemClicked -> TODO()
+            is FeedScreenEvent.SearchQueryChanged -> onSearchQueryChanged(event.newSearchQuery)
+        }
     }
+
+    private fun onSearchQueryChanged(newQuery: String) {
+        _state.update { it.copy(searchQuery = newQuery) }
+
+        viewModelScope.launch {
+            val filteredNews = withContext(Dispatchers.Default) {
+                news.filter{it.title.contains(newQuery)}
+            }
+            _state.update { it.copy(filteredNews = filteredNews) }
+        }
+
+
+    }
+
+}
+
+@AssistedFactory
+interface Factory {
+    fun create(navigate: (Screen) -> Unit): FeedScreenViewModel
+}
 
 }
